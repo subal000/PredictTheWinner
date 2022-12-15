0a05802769fbafe0fd5a1e2e5b97037388961829
subal000 <subal.ghimire2396@gmail.com>
2022-12-15 17:51:06 +0545

    Fixed error in StartPredicting.kt

--- app/src/main/java/com/example/android/myapplication/ui/StartPredicting.kt
+++ app/src/main/java/com/example/android/myapplication/ui/StartPredicting.kt
@@ -22,15 +22,13 @@ class StartPredicting : AppCompatActivity(), KodeinAware {
         setContentView(R.layout.start_predicting)
         viewModel = ViewModelProvider(this, factory).get(PredictionViewModel::class.java)
         val semiButton = findViewById(R.id.semiFinalButton) as Button
-        semiButton.setOnClickListener(View.OnClickListener {
-            Log.v("Semi","clicked")
-            val semiTeam1 = findViewById<TextView>(R.id.semi1).text.toString()
+        semiButton.setOnClickListener(View.OnClickListener { val semiTeam1 = findViewById<TextView>(R.id.semi1).text.toString()
             val semiTeam2 = findViewById<TextView>(R.id.semi2).text.toString()
-            val finalRadioGroup = findViewById<RadioGroup>(R.id.radioGroupSemi)
-            val id: Int = finalRadioGroup.checkedRadioButtonId
-            if (id!=-1){ // If any radio button checked from radio group
+            val semiRadioGroup = findViewById<RadioGroup>(R.id.radioGroupSemi)
+            val idSemi: Int = semiRadioGroup.checkedRadioButtonId
+            if (idSemi!=-1){ // If any radio button checked from radio group
                 // Get the instance of radio button using id
-                val radio: RadioButton = findViewById(id)
+                val radio: RadioButton = findViewById(idSemi)
                 val yourSemiPred = radio.text.toString()
                 val item = Predicteditem(yourSemiPred,semiTeam1,semiTeam2)
                 viewModel.addPrediction(item)
@@ -39,17 +37,16 @@ class StartPredicting : AppCompatActivity(), KodeinAware {
                 Toast.makeText(applicationContext,"Please select any one option:" +
                         " nothing has been selected",
                     Toast.LENGTH_SHORT).show()
-            }
+            } })
         val finalButton = findViewById(R.id.finalButton) as Button
         finalButton.setOnClickListener(View.OnClickListener {
-            Log.v("final","clicked")
             val finalTeam1 = findViewById<TextView>(R.id.final1).text.toString()
             val finalTeam2 = findViewById<TextView>(R.id.final2).text.toString()
             val finalRadioGroup = findViewById<RadioGroup>(R.id.radioGroupFinal)
-            val id: Int = finalRadioGroup.checkedRadioButtonId
-            if (id!=-1){ // If any radio button checked from radio group
+            val idFinal: Int = finalRadioGroup.checkedRadioButtonId
+            if (idFinal!=-1){ // If any radio button checked from radio group
                 // Get the instance of radio button using id
-                val radio: RadioButton = findViewById(id)
+                val radio: RadioButton = findViewById(idFinal)
                 val yourFinalPred = radio.text.toString()
                 val item = Predicteditem(yourFinalPred,finalTeam1,finalTeam2)
                 viewModel.addPrediction(item)
@@ -58,9 +55,6 @@ class StartPredicting : AppCompatActivity(), KodeinAware {
                 Toast.makeText(applicationContext,"Please select any one option:" +
                         " nothing has been selected",
                     Toast.LENGTH_SHORT).show()
-            }
-
-        })
-    })
+            } })
 }
 }
